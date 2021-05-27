6dffd67783773182afb8b378fe9f5088433970cd
jshuang0520 <s9140911@yahoo.com.tw>
2021-05-28 01:12:49 +0800

    fix: 0001_two_sum

--- python_part/hash_table/0001_two_sum.py
+++ python_part/hash_table/0001_two_sum.py
@@ -16,16 +16,42 @@ class Solution:
 
     @timeit
     def run(self, nums: List[int], target: int) -> List[int]:
+        """
         for i in range(0, len(nums)-1):
             tmp = target - nums[i]
-            # nums.remove(nums[i])
-            new_nums = nums[:i] + nums[i+1 :]
+            # nums.remove(nums[i])  # FIXME: just record the mistake I made
+            new_nums = nums[:i] + nums[i+1:]
             self.logger.info("new_nums: {}".format(new_nums))
             new_set = set(new_nums)
             if tmp in new_set:
                 return [i, nums.index(tmp)]
-            else:
-                pass
+        --
+        Wrong Answer
+
+        Input:[3,3], 6
+        Output:[0,0]
+        Expected:[0,1]
+        """
+
+        """
+        description
+        --
+        
+        You can return the answer in any order.
+        -> think about dict() or set()
+        --
+        
+        result:
+        Runtime: 40 ms, faster than 95.52% of Python3 online submissions for Two Sum.
+        Memory Usage: 15.4 MB, less than 7.40% of Python3 online submissions for Two Sum.
+        """
+        lookup = {}
+        for idx, num in enumerate(nums):
+            if target - num in lookup:
+                self.logger.info('lookup: {}'.format(lookup))
+                # return [idx, lookup[num]]  # FIXME: just record the mistake I made
+                return [lookup[target - num], idx]
+            lookup[num] = idx
 
 
 if __name__ == '__main__':
