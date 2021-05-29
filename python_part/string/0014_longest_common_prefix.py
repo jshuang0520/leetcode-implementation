ad87f823d79870b76c76f8486cd47e06f7f2debb
jshuang0520 <s9140911@yahoo.com.tw>
2021-05-29 19:48:09 +0800

    chore: add comments for 0014_longest_common_prefix

--- python_part/string/0014_longest_common_prefix.py
+++ python_part/string/0014_longest_common_prefix.py
@@ -20,16 +20,30 @@ class Solution:
     @timeit
     def run(self, strs: List[str]) -> str:
         """
+        reference
+        google: python list of string common prefix
+        https://stackoverflow.com/questions/61788169/how-to-get-common-prefix-of-strings-in-a-list
+        --
+
         Runtime: 36 ms, faster than 50.89% of Python3 online submissions for Longest Common Prefix.
         Memory Usage: 14.2 MB, less than 80.41% of Python3 online submissions for Longest Common Prefix.
         """
+        '''
+        log printed:
+        s: ('f', 'f', 'f')
+        s: ('l', 'l', 'l')
+        ------------------- -> the conditional flow breaks here
+        s: ('o', 'o', 'i')
+        s: ('w', 'w', 'g')
+        '''
         res = list()
-        for s in zip(*strs):
-            self.logger.info('s: {}'.format(s))
+        for s in zip(*strs):  # FIXME: the usage of zip for all elements in a list, check the log printed above
             if len(set(s)) == 1:
+                self.logger.info('s: {}'.format(s))
                 res.append(s[0])
             else:
                 break
+
         if len(res) == 0:
             return ''
         else:
